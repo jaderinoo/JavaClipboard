947c9369ceaba2a740b66a38cb9d2449b6e4325b
Jad El-Khatib <jaderinoo@gmail.com>
2019-06-12 20:48:20 -0400

    Basic copy functionality

--- JavaClipboard/src/Main.java
+++ JavaClipboard/src/Main.java
@@ -1,4 +1,27 @@
+import java.awt.datatransfer.*;
+import java.io.IOException;
+import java.awt.*;
 
 public class Main {
- //Test Commit
+	public static void main(String[] args)throws Exception
+    {
+		clippy();
+		
+    }
+	public static void clippy() throws UnsupportedFlavorException, IOException {
+		String[] clippy = new String[10];
+	        // Create a Clipboard object using getSystemClipboard() method
+	        Clipboard c=Toolkit.getDefaultToolkit().getSystemClipboard();
+	        
+	        // Set Clipboard to current clippy
+	        String current = (String) (c.getData(DataFlavor.stringFlavor));
+	        
+	        // Pushes the current to clippy and moves the previous to next
+	        clippy[0] = current;
+	        
+	        // Get data stored in the clipboard that is in the form of a string (text)
+	        for (int i = 0; i < 5; i++) {
+	        System.out.println("Clippy " + (i+1) + ": \n" + clippy[i] + "\n");
+        }
+	}
 }

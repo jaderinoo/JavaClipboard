066919c954f0eff03c47ea7c881d21b459a44844
Jad El-Khatib <jaderinoo@gmail.com>
2019-06-12 21:12:13 -0400

    Added a Jframe for the console

--- JavaClipboard/src/Main.java
+++ JavaClipboard/src/Main.java
@@ -1,15 +1,29 @@
 import java.awt.datatransfer.*;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
 import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
 	public static void main(String[] args)throws Exception
     {
+		Console console = new Console();
+	    console.init();
+	    Main launcher = new Main();
+	    launcher.setVisible(false);
+	    console.getFrame().setLocation(
+	    launcher.getX() + launcher.getWidth() + launcher.getInsets().right,
+	    launcher.getY());
 		clippy();
 		
     }
+	
+
+	
 	public static void clippy() throws UnsupportedFlavorException, IOException {
 		String[] clippy = new String[10];
+		int counter = 0;
 	        // Create a Clipboard object using getSystemClipboard() method
 	        Clipboard c=Toolkit.getDefaultToolkit().getSystemClipboard();
 	        
@@ -17,22 +31,20 @@ public class Main {
 	        String current = (String) (c.getData(DataFlavor.stringFlavor));
 	        
 	        // Pushes the current to clippy and moves the previous to next
-	        for (int i = 0; i < 10; i++) {
-	        if(clippy[i] != null) {
-	        	clippy[i+1] = current;
-	        	break;
-	        }else {
-	        	clippy[i] = current;
-	        	break;
-	        }
-	        }
+	        	clippy[counter] = current;
+
 	        
 	        
 	        // Get data stored in the clipboard that is in the form of a string (text)
 	        for (int i = 0; i < 10; i++) {
 	        System.out.println("Clippy " + (i+1) + ": \n" + clippy[i] + "\n");
         }
-	        if()
-	        clippy();
+	       while(current != clippy[0]){
+	    	   try {
+	    		   TimeUnit.SECONDS.sleep(1);
+	    		   counter++;
+	    		   clippy();
+	    	   } catch (Exception e) {}
+	       }
 	}
 }
